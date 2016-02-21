d48596827991ff3eb0113698a6e4b482d944f866
Dan McClain <git@danmcclain.net>
2016-02-21 00:11:46 -0500

    Release config, travis caching

--- mix.exs
+++ mix.exs
@@ -3,30 +3,33 @@ defmodule VerifyOrigin.Mixfile do
 
   def project do
     [app: :verify_origin,
-     version: "0.0.1",
+     version: "0.1.0",
      elixir: "~> 1.2",
+     name: "VerifyOrigin",
+     description: "A library for using Origin header checking to prevent CSRF",
+     source_url: "https://github.com/danmcclain/verify_origin",
+     package: package,
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [{:plug, "~> 1.0"}]
+  end
+
+  defp package do
+    [
+      contributors: ["Dan McClain"],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/danmcclain/verify_origin",
+        "Documentation" => "http://hexdocs.pm/verify_origin"
+      }
+    ]
   end
 end
